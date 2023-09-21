ce7d5a0e36c2b412c5742e14856fade89a7da75c
elena <elenastarck1992@mail.ru>
2023-09-21 21:01:05 +0300

    try/except added

--- src/video.py
+++ src/video.py
@@ -6,18 +6,26 @@ class Video:
     api_key = os.getenv('YT_API_KEY')
 
     def __init__(self, video_id: str):
-        self.video_id = video_id
-        self.video = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                      id=video_id).execute()
-        self.title_video = self.video['items'][0]['snippet']['title']
-        self.view_count = self.video['items'][0]['statistics']['viewCount']
-        self.video_url = f"https://www.youtube.com/watch?v={video_id}"
-        self.like_count = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.video = self.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                          id=video_id).execute()
+            self.title = self.video['items'][0]['snippet']['title']
+            self.view_count = self.video['items'][0]['statistics']['viewCount']
+            self.video_url = f"https://www.youtube.com/watch?v={video_id}"
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
+        except:
+            print("Введен некорректрый ID видео.")
+            self.video_id = video_id
+            self.video = None
+            self.title = None
+            self.view_count = None
+            self.video_url = None
+            self.like_count = None
 
     @classmethod
     def get_service(cls):
         return build('youtube', 'v3', developerKey=cls.api_key)
-
 
     def __str__(self):
         return f'{self.title_video}'
